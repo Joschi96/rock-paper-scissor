21859191ab97561081796f42d14fa696c4769cdc
Joschi96 <117238956+Joschi96@users.noreply.github.com>
2024-08-21 19:01:28 +0200

    Added basic UI

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let humanScore = 0;
+let computerScore = 0;
+
+const buttons = document.querySelectorAll('input');
+
 // Returns a random Computer Choice
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random()*3);
@@ -19,35 +24,60 @@ function getComputerChoice() {
 function getHumanChoice() {
     let humanChoice = prompt("Choose Rock, Paper or Scissors").toLowerCase();
     if (humanChoice=="rock"||humanChoice=="paper"|| humanChoice=="scissors") {
-        return console.log(humanChoice);
+        return humanChoice;
     } else{
         alert("Wrong input. Please choose one of the valid choices!");
         getHumanChoice();
     }
 }
 
-//getHumanChoice();
-//getComputerChoice();
-
-let humanScore = 0;
-let computerScore = 0;
-
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice=== computerChoice) {
-        console.log("It's a tie!");
-    } else if (
-        humanChoice == "rock" && computerChoice == "scissors" ||
-        humanChoice == "scissors" && computerChoice == "papers" ||
-        humanChoice == "paper" && computerChoice == "rock"
-    ){
-        console.log("You won!");
-        humanScore++;
-    } else {
-        console.log("You lost!");
-        computerScore++;
-    }
+function disableButtons(){
+    buttons.forEach(button => {
+        button.disabled = true;
+    });
 }
 
-const humanSelection =getHumanChoice();
-const computerSelection = getComputerChoice();
-playRound(humanSelection,computerSelection);
+function playRound(humanChoice) {
+    let computerSelection = getComputerChoice();
+    let result = '';
+
+        if (humanChoice === computerSelection) {
+            result = ('It\'s a tie. You both chose ' + humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)+ 
+                '.<br><br> Player score: ' + humanScore + '<br>Computer Score: '
+                + computerScore);
+        } 
+        else if (
+            humanChoice == "rock" && computerSelection == "scissors" ||
+            humanChoice == "scissors" && computerSelection == "papers" ||
+            humanChoice == "paper" && computerSelection == "rock"
+        ){
+            humanScore++;
+            result = ('You won! ' + humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1) 
+                + ' beats '+ computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1) +
+                '!<br><br> Player score: ' + humanScore + '<br>Computer Score: '
+                + computerScore);
+
+            if (humanScore==5) {
+                result += '<br><br>You won the game! Reload page to play again!'
+                disableButtons();
+            }
+        } 
+        else {
+            computerScore++;
+            result = ('You lost! ' + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1) + ' beats '
+                + humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1) +
+                '!<br><br>Player score: ' + humanScore + '<br>Computer Score: '
+                + computerScore);
+            if (computerScore==5) {
+                result += '<br><br>You lost the game! Reload page to play again!'
+                disableButtons();
+            }
+        }
+        document.querySelector('.score').innerHTML = result;
+} 
+
+buttons.forEach(button => {
+    button.addEventListener("click", ()=>{
+        playRound(button.value.toLowerCase());
+    })
+});
